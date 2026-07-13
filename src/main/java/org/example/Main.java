8a58b094bcb97315fa33f83b2de1f5faa5db1e9e
Nicolas Moreyra <nmoreyra90@gmail.com>
2026-07-12 22:44:51 -0300

    Fase 2 completada: instanciación de objetos y prueba de ejecución limpia

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,13 @@
 package org.example;
 
 import org.example.domain.Track;
+import org.example.domain.Driver;
 
 public class Main {
     public static void main(String[] args) {
         Track rafaela = new Track(1L, "Autódromo Ciudad de Rafaela", "Rafaela", 4740);
         System.out.println("Pista cargada: " + rafaela.getName() + " - Longitud: " + rafaela.getLengthInMeters() + " metros.");
+        Driver piloto = new Driver(1L, "Mariano Werner", 37);
+        System.out.println("Piloto inscrito: " + piloto.getName());
     }
 }
-Driver piloto = new Driver("Mariano Werner", 37);
-System.out.println("Piloto inscrito: " + piloto.getName());
-
