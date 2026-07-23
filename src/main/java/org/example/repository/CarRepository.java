13a894573a9ce50fd5bdebc1c49b84f87485a5d7
Nicolas Moreyra <nmoreyra90@gmail.com>
2026-07-23 00:17:31 -0300

    Backup estable antes de refactorizar al ChampionshipService

--- src/main/java/org/example/repository/CarRepository.java
+++ src/main/java/org/example/repository/CarRepository.java
@@ -1,7 +1,8 @@
 package org.example.repository;
-import org.example.domain.Championship;
+import org.example.domain.CarPolimorfico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChampionshipRepository extends JpaRepository<Championship, Long> {}
+public interface CarRepository extends JpaRepository<CarPolimorfico, Long> {
+}
--- src/main/java/org/example/repository/TrackRepository.java
+++ src/main/java/org/example/repository/TrackRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TrackRepository extends JpaRepository<Track, Long> {}
+public interface TrackRepository extends JpaRepository<Track, Long> {
+}
